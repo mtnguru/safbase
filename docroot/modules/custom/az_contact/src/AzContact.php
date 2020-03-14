254258e5aff094653d87f5fb8ff37b7f051338b3
James Sorensen <james@ecosleuth.com>
2020-03-13 18:16:07 -0600

    Investor - Approved

--- docroot/modules/custom/az_contact/src/AzContact.php
+++ docroot/modules/custom/az_contact/src/AzContact.php
@@ -35,13 +35,13 @@ class AzContact {
 
     $query->leftJoin('contact_message__field_interest', 'cmfi', 'cm.id = cmfi.entity_id');
     $query->leftJoin('taxonomy_term_field_data', 'ttfd', 'cmfi.field_interest_target_id = ttfd.tid');
-    $query->addField('cmfi', 'field_interest_target_id', 'interest id');
+    $query->addField('cmfi', 'field_interest_target_id', 'interestId');
     $query->addField('ttfd', 'name', 'interest');
 
 //$query->condition('nfd.status', $set['status'], (is_array($set['status'])) ? 'IN' : '=');
 
     $totalRows = $query->countQuery()->execute()->fetchField();
-    $results = $query->execute()->fetchAllAssoc('id');
+    $results = $query->execute()->fetchAll();
 
     return [
       'results' => $results,
@@ -53,6 +53,7 @@ class AzContact {
   static public function queryEmail($email) {
     $query = \Drupal::database()->select('node__field_email', 'nfm');
     $query->addField('nfm', 'field_email_value', 'mail');
+    $query->addField('nfm', 'entity_id', 'id');
     $query->condition('nfm.field_email_value', $email, (is_array($email)) ? 'IN' : '=');
     $results = $query->execute()->fetch();
 
@@ -62,23 +63,18 @@ class AzContact {
     ];
   }
 
-  static public function createContact($row) {
-    /*
-    $node = Node::create([
-      'type' => 'article_internal',
-      'title' => 'shit',
-      'field_media' => '5',
-    ]);
-    $node->save();
-    */
+  static public function createContact($row, $source) {
 
     $node = Node::create([
       'type'           => 'contact',
       'status'         => 1,
       'title'          => $row->name,
       'field_email'    => $row->mail,
-      'field_interest' => [$row->interestid],
+      'field_interest' => [$row->interestId],
       'field_phone'    => $row->phone,
+      'field_source'   => $source,
+      'field_first_contact_date' => date('Y-m-d', $row->created),
+
       'field_address'  => [
         'address_line1' => $row->line1,
         'address_line2' => $row->line2,
@@ -90,26 +86,30 @@ class AzContact {
     ]);
     $node->save();
     return $node;
-
-    /*
-    $row->id;
-    $row->name;        title
-    $row->mail;        field_email
-    $row->created;     field_first_contact_date
-    $row->line1;       field_address
-    $row->line2;       field_address
-    $row->locality;    field_address
-    $row->area;        field_address
-    $row->code;        field_address
-    $row->country;     field_address
-    $row->phone;       field_phone
-    $row->interestid;  field_interest
-    $row->interest;
-    */
-    return null;
   }
 
   static public function addSubmission($contactId, $data) {
+    // So what to do here?  Overwrite existing data, that way it gets corrected?
+    // Read in the contact;
+    $contact = \Drupal::entityTypeManager()->getStorage('node')->load($contactId);
+
+    if ($data->interestId) {
+      $existingInterests = array_column($contact->field_interest->getValue(), 'target_id');
+      if (!in_array($data->interestId, $existingInterests)) {
+        $contact->field_interest[] = $data->interestId;
+      }
+    }
+
+    if ($data->name) {
+      $contact->setTitle($data->name);
+    }
+
+    if ($data->phone) {
+      $contact->field_phone->setValue($data->phone);
+    }
+
+    $contact->save();
+    return;
   }
 }
 
